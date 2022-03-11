a439a30018b87b6ef9970b2dfff7d13882527841
Viennivre <echefu.vivian@gmail.com>
2022-03-11 10:48:42 +0100

    another commit

---
+++ howtocreateadirectoryoncmd.py
@@ -0,0 +1,4 @@
+#search for cmd on start panel
+#type dir and then type cd followed bythe folder name you want the folder to appear under
+#then type mkdir followed by the name of the folder, note that if you want to creae a folder with a name that has space in it use a quotation mark to avoid creating two different folders
+#to delete type rmdir followed by the name of the folder
